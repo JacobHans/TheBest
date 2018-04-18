cb60f9f33d6ec09bc8683d514ed1a811af3e3d99
lamc8684 <lamc8684@colorado.edu>
2018-04-17 21:30:28 -0600

    more tests

--- Hartlands_Chat/tests/test_app.py
+++ Hartlands_Chat/tests/test_app.py
@@ -2,15 +2,33 @@ import pytest
 import app
 import os
 import tempfile
+from flask import Flask, flash, redirect, render_template, request, session, abort
+
 
 db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
+application = app.app.test_client()
 
 def test_setUp():
     app.app.testing = True
-    application = app.app.test_client()
-    #with app.app.app_context():
-		#app.init_db()
 		
 def test_tearDown():
 	os.close(db_fd)
 	os.unlink(app.app.config['DATABASE'])
+	
+def test_empty_db():
+	rv = application.get('/')
+	assert rv.data 
+	
+def login(username, password):
+	return application.post('/login', data=dict(
+	    username=username,
+	    password=password
+	), follow_redirects=True)
+	
+def test_login():
+	rv = login('admin', 'default')
+	assert rv.data
+	rv = login('admin', 'defaultx')
+	assert rv.data
+	
+	
